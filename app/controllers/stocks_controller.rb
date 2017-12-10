6cfe0d8fc93e141c7853eee24c6bb21e4a74d2aa
Nikolaos Ouvenis <nokid_gr@hotmail.it>
2017-12-10 15:07:17 +0200

    added JS for lookup rendering

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,20 @@
 class StocksController < ApplicationController
 
   def search
-    @stock = Stock.new_from_lookup(params[:stock])
-    render 'users/my_portfolio'
-  end
+    if params[:stock].present?
+      @stock = Stock.new_from_lookup(params[:stock])
+      if @stock
+        respond_to do |format|
+          format.js {render partial: 'users/result' }
+        end
+      else
+        flash[:danger] = "You have entered an incorrect symbol"
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:danger] = "Empty search screen"
+      redirect_to my_portfolio_path
+    end #if end
+  end #search end
 
-end
+end #class end
