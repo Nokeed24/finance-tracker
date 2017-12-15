a24c8e8d13b6b1bcab313161dc3472da2e07dbac
Nokeed-Linux <nokid_gr@hotmail.it>
2017-12-15 12:43:03 +0200

    user search errors handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,16 @@ class UsersController < ApplicationController
     @friendships = current_user.friends
   end
 
+  def search
+    if params[:search_param].blank?
+      flash.now[:danger] = "You have searched for an empty string"
+    else
+      @users = User.search(params[:search_param])
+      flash.now[:danger] = "No users match the search criteria" if @users.blank?
+    end #if end
+    respond_to do |format|
+      format.js { render partial: 'friends/result' }
+    end #respond end
+  end
+
 end
